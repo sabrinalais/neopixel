8853e9eed80928a5b088df61fabb47028f44ed1f
sabrinalais <sabrinalaisvieiraramos@gmail.com>
2025-02-14 19:55:50 -0300

    Adicionando comentários de observação

--- neopixel_pio.c
+++ neopixel_pio.c
@@ -11,7 +11,8 @@
 #include "hardware/pwm.h"
 #include "ws2818b.pio.h" // Biblioteca gerada pelo arquivo .pio durante compilação.
 
-
+/** Obs: os dados foram obtidos aleatoriamente por meio da função rand(), 
+ * pois o sensor estava indisponível para a execução do projeto*/
 
 const uint I2C_SDA = 14;
 const uint I2C_SCL = 15;
